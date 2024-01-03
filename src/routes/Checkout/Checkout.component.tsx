9fa3d508006ae733c7e6e5120a417a19e6777fe2
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2024-01-03 00:11:03 +0000

    3-1-24 12:10am

--- src/routes/Checkout/Checkout.component.tsx
+++ src/routes/Checkout/Checkout.component.tsx
@@ -10,7 +10,8 @@ import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs.component"
 import CheckoutProductPayment from "./CheckoutProductPayment/CheckoutProductPayment.component"
 import EmptyCheckout from "./EmptyCheckout/EmptyCheckout.component"
 import PageBanner from "../../components/PageBanner/PageBanner.component"
-import CheckoutOrder from "./CheckoutOrder/CheckoutOrder.component"
+
+import emptyBasket from "../../../assets/images/basket-icon.png"
 
 interface CartItem {
   calories: number
@@ -40,9 +41,69 @@ const Checkout: FC = () => {
           setUserLatestOrder={setUserLatestOrder}
         />
       ) : showConfirmation ? (
-        <CheckoutOrder
-          userLatestOrder={userLatestOrder}
-        />
+        <div className="container">
+          <div className="order-stamp">
+            <h2
+              className="order-stamp-heading"
+            >
+              Congrats, User
+            </h2>
+            <img
+              alt="Basket Icon"
+              className="order-stamp-image"
+              src={emptyBasket}
+            />
+            <p>
+              Thank you for your recent order. We are pleased to confirm that we have received your order and it is currently being processed.
+            </p>
+            <span
+              className="order-stamp-number"
+            >
+              Order Number: 123456789
+            </span>
+          </div>
+          <div className="order-summary">
+            <h2
+              className="order-summary-heading"
+            >
+              Order Summary
+            </h2>
+          </div>
+          <div className="cart-container">
+            <div className="cart-table">
+              <div className="cart-table-header">
+                <div className="cart-table-third">Product Name</div>
+                <div className="cart-table-third">Quantity</div>
+                <div className="cart-table-third">Price</div>
+              </div>
+              <div className="cart-table-content">
+                {userLatestOrder?.map((orderItem, index) => (
+                  <div
+                    className="cart-table-item"
+                    key={index+1}
+                  >
+                    <div className="cart-table-third">
+                      <h4 className="cart-order-product">
+                        {orderItem.name}
+                      </h4>
+                    </div>
+                    <div
+                      className="cart-table-third cart-table-order-qty"
+                    >
+                      {`x${orderItem.quantity}`}
+                    </div>
+                    <div
+                      className="cart-table-third cart-table-price"
+                    >
+                      {`£${orderItem.price.toFixed(2)}`}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="total">Total: £7.70</div>
+          </div>
+        </div>
       ) : (
         <EmptyCheckout />
       )}
